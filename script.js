4c3ae2cab4ede2a4fa733cf5c3df13389085232e
Abdul Mazar <abdulmazar.dev@gmail.com>
2026-08-02 11:51:35 +0530

    Add total points display for red and blue teams

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let redPoint = 0;
-let bluePoint = 0;
+let redPoint = bluePoint = totalRed = totalBlue = 0;
+
+let totalRedPoints = document.getElementById("totalRedPoints");
 let redPrePoint = document.getElementById("redTeamPoints");
-let bluePrePoint = document.getElementById("blueTeamPoints");
 let redPointDisplay = document.getElementById("red-points");
+
+let totalBluePoints = document.getElementById("totalBluePoints");
+let bluePrePoint = document.getElementById("blueTeamPoints");
 let bluePointDisplay = document.getElementById("blue-points");
 
 function addRedPoint() {
@@ -17,6 +20,10 @@ function addBluePoint() {
 function savePoint() {
   redPrePoint.textContent += " - " + redPoint;
   bluePrePoint.textContent += " - " + bluePoint;
+  totalRed += redPoint;
+  totalBlue += bluePoint;
+  totalRedPoints.textContent = totalRed;
+  totalBluePoints.textContent = totalBlue;
   redPoint = bluePoint = 0;
   redPointDisplay.textContent = bluePointDisplay.textContent = 0;
 }
