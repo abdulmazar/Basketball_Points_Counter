8644e3e9b015d63cd950e10e16d129557561f94a
Abdul Mazar <abdulmazar.dev@gmail.com>
2026-08-03 20:36:57 +0530

    Implemented objects to store the points and teams

--- script.js
+++ script.js
@@ -1,58 +1,92 @@
-let redPoint = bluePoint = totalRed = totalBlue = 0;
+let red = {
+  name: document.getElementById("team1"),
+  points: {
+    value: 0,
+    display: document.getElementById("red-points"),
+  },
+  totalPoints: {
+    value: 0,
+    display: document.getElementById("totalRedPoints"),
+  },
+  prePoints: document.getElementById("redTeamPoints"),
+};
 
-let totalRedPoints = document.getElementById("totalRedPoints");
-let redPrePoint = document.getElementById("redTeamPoints");
-let redPointDisplay = document.getElementById("red-points");
-
-let totalBluePoints = document.getElementById("totalBluePoints");
-let bluePrePoint = document.getElementById("blueTeamPoints");
-let bluePointDisplay = document.getElementById("blue-points");
-
-let team1 = document.getElementById("team1");
-let team2 = document.getElementById("team2");
+let blue = {
+  name: document.getElementById("team2"),
+  points: {
+    value: 0,
+    display: document.getElementById("blue-points"),
+  },
+  totalPoints: {
+    value: 0,
+    display: document.getElementById("totalBluePoints"),
+  },
+  prePoints: document.getElementById("blueTeamPoints"),
+};
 
 function inputTeamNames() {
-  team1.textContent = prompt("Enter Team Red Name");
-  team2.textContent = prompt("Enter Team Blue Name");
+  red.name.textContent = prompt("Enter Team Red Name");
+  blue.name.textContent = prompt("Enter Team Blue Name");
 }
 
 inputTeamNames();
 
 function addRedPoint() {
-  redPoint++;
-  redPointDisplay.textContent = redPoint;
+  red.points.value++;
+  red.points.display.textContent = red.points.value;
 }
 function addBluePoint() {
-  bluePoint++;
-  bluePointDisplay.textContent = bluePoint;
+  blue.points.value++;
+  blue.points.display.textContent = blue.points.value;
 }
 
 function savePoint() {
-  redPrePoint.textContent += " - " + redPoint;
-  bluePrePoint.textContent += " - " + bluePoint;
-  totalRed += redPoint;
-  totalBlue += bluePoint;
-  totalRedPoints.textContent = totalRed;
-  totalBluePoints.textContent = totalBlue;
-  redPoint = bluePoint = 0;
-  redPointDisplay.textContent = bluePointDisplay.textContent = 0;
+  red.prePoints.textContent += " - " + red.points.value;
+  blue.prePoints.textContent += " - " + blue.points.value;
+  red.totalPoints.value += red.points.value;
+  blue.totalPoints.value += blue.points.value;
+  red.totalPoints.display.textContent = red.totalPoints.value;
+  blue.totalPoints.display.textContent = blue.totalPoints.value;
+  red.points.value = blue.points.value = 0;
+  red.points.display.textContent = blue.points.display.textContent = 0;
 }
 
-function endGame(){
-  if(totalRed > totalBlue){
-    alert(team1.textContent + " Wins! " + " With the Lead Of " + (totalRed-totalBlue) + " Points from " + team2.textContent);
-  } else if(totalBlue > totalRed){
-    alert(team2.textContent + " Wins!" + " With the Lead Of " + (totalBlue-totalRed) + " Points from " + team1.textContent);
+function endGame() {
+  if (red.totalPoints.value > blue.totalPoints.value) {
+    alert(
+      red.name.textContent +
+        " Wins! " +
+        " With the Lead Of " +
+        (red.totalPoints.value - blue.totalPoints.value) +
+        " Points from " +
+        blue.name.textContent,
+    );
+  } else if (blue.totalPoints.value > red.totalPoints.value) {
+    alert(
+      blue.name.textContent +
+        " Wins!" +
+        " With the Lead Of " +
+        (blue.totalPoints.value - red.totalPoints.value) +
+        " Points from " +
+        red.name.textContent,
+    );
   } else {
     alert("It's a tie!");
   }
-  redPoint = bluePoint= totalRed = totalBlue = 0;
-  redPointDisplay.textContent = bluePointDisplay.textContent = totalRedPoints.textContent = totalBluePoints.textContent = 0;
-  redPrePoint.textContent = bluePrePoint.textContent = "";
+  red.points.value =
+    blue.points.value =
+    red.totalPoints.value =
+    blue.totalPoints.value =
+      0;
+  red.points.display.textContent =
+    blue.points.display.textContent =
+    red.totalPoints.display.textContent =
+    blue.totalPoints.display.textContent =
+      0;
+  red.prePoints.textContent = blue.prePoints.textContent = "";
 
   let userAgreed = confirm("Do you want to change the team names?");
-  if(userAgreed){
+  if (userAgreed) {
     inputTeamNames();
-  } 
-
+  }
 }
